d542585d3dfc2cf2b75b8b4541d70af58b7c0d74
Ostin Solo <contact@ostinsolo.co.uk>
2024-10-16 22:42:53 +0100

    rules fix, logic update

--- ai_gen/ableton_data.py
+++ ai_gen/ableton_data.py
@@ -1,5 +1,5 @@
 # ableton_data.py
-
+from .utterance_actions_template import action_order_templates, utterance_templates
 # List of all available audio effects in Ableton Live
 audio_effects = [
     "Amp",
@@ -205,57 +205,6 @@ instruments = [
     "Wavetable"
 ]
 
-# Common actions that can be performed in Ableton Live
-common_actions = [
-    "add",
-    "remove",
-    "change",
-    "adjust",
-    "increase",
-    "decrease",
-    "mute",
-    "unmute",
-    "solo",
-    "unsolo",
-    "record",
-    "stop",
-    "play",
-    "pause",
-    "continue",
-    "resume",
-    "bypass",
-    "unbypass",
-    "freeze",
-    "unfreeze",
-    "group",
-    "duplicate",
-    "delete",
-    "rename",
-    "copy",
-    "paste",
-    "set",
-    "apply",
-    "arm",
-    "listen",
-    "search"
-]
-
-# Specific actions or commands unique to Ableton Live
-specific_actions = [
-    "Control",
-    "Map",
-    "Delete Map",
-    "Min Range",
-    "Max Range",
-    "search device",
-    "Hey asa",
-    "Device",
-    "Device Chain",
-    "View Clip",
-    "delete device",
-    "Arranger",
-    "Session"
-]
 
 # List of parameters that can be adjusted for various devices
 parameters = [
@@ -299,37 +248,53 @@ track_types = [
 
 # Categorized actions for different elements in Ableton Live
 actions = {
+    "track_creation_actions": [
+        "add",
+        "create"
+    ],
     "track_actions": [
+        "remove",
         "arm",
         "solo",
+        "unsolo",
         "mute",
+        "unmute",
         "rename",
         "delete",
         "duplicate",
         "freeze",
         "unfreeze",
         "group",
-        "volume",
-        "pan",
-        "color"
+        "bypass",
+        "unbypass",
+        "set volume",
+        "set pan",
+        "set color"
     ],
     "project_actions": [
-        "create track",
-        "create return track",
         "create scene",
         "delete scene",
         "duplicate scene",
         "set tempo",
         "set time signature",
         "set loop start",
-        "set loop end"
+        "set loop end",
+        "record",
+        "stop",
+        "play",
+        "pause",
+        "continue",
+        "resume"
     ],
     "device_actions": [
         "add",
         "remove",
+        "change",
         "enable",
         "disable",
-        "parameter",
+        "bypass",
+        "unbypass",
+        "set parameter"
     ],
     "clip_actions": [
         "create",
@@ -343,100 +308,137 @@ actions = {
         "unwarp",
         "transpose",
         "reverse",
-        "view clip"
+        "view",
+        "record",
+        "stop",
+        "play",
+        "pause",
+        "freeze",
+        "unfreeze"
     ],
     "view_actions": [
-        "Arranger",
-        "Session",
-        "Device Chain"
+        "show Arranger",
+        "show Session",
+        "show Device Chain"
     ],
     "mapping_actions": [
         "Map",
         "Delete Map",
-        "Min Range",
-        "Max Range",
+        "Set Min Range",
+        "Set Max Range",
         "Control"
-    ]
-}
-
-# Activation command
-activation_command = "Hey asa"
-
-# Dictionary of speed modifiers for control actions
-speed_modifiers = {
-    "instant": ["now", "immediately"],
-    "fast": ["fast", "quick", "rapidly"],
-    "slow": ["slow", "gradually", "smoothly"]
+    ],
+    "value_actions": [
+        "set",
+        "increase",
+        "decrease"
+    ],
+    "speed_modifiers": {
+        "instant": ["now", "immediately"],
+        "fast": ["fast", "quick", "rapidly"],
+        "slow": ["slow", "gradually", "smoothly"]
+    }
 }
 
 # Templates for generating action orders (sequences of actions)
-# Rules:
-# 1. Track-related actions always start with "track {track_number}" unless it's a global action
-# 2. Device-related actions always start with "search device" followed by the device name
-# 3. For instruments, the order is: "search device", "{instrument}", "{device_type}", then the action
-# 4. Audio effects are treated separately from instruments
-# 5. Control actions include one of the speed modifiers from the speed_modifiers dictionary,
-#    followed by a value between 0 and 100
-# 6. When creating a track, don't mention a track number (it doesn't exist yet)
-action_order_templates = [
-    ["track {track_number}", "{track_action}"],
-    ["track {track_number}", "search device", "{audio_effect}", "{device_action}"],
-    ["track {track_number}", "search device", "{instrument}", "{device_type}", "{device_action}"],
-    ["{project_action}", "{track_type}"],
-    ["track {track_number}", "clip {clip_number}", "{clip_action}"],
-    ["track {track_number}", "duplicate", "search device", "{audio_effect}", "add"],
-    ["track {track_number1}", "track {track_number2}", "group", "search device", "{audio_effect}", "add"],
-    ["track {track_number}", "create_send", "search device", "{audio_effect}"],
-    ["track {track_number}", "freeze"],
-    ["track {track_number}", "unfreeze", "flatten"],
-    ["create_track", "{track_type}", "search device", "{instrument}", "{device_type}"],
-    ["track master", "search device", "{audio_effect}", "add"],
-    ["create_return_track", "search device", "{audio_effect}", "add"],
-    ["track {track_number1}", "search device", "{audio_effect}", "copy", "track {track_number2}"],
-    ["group_tracks", "{instrument}"],
-    ["create_rack", "search device", "{audio_effect1}", "add", "search device", "{audio_effect2}", "add", "search device", "{audio_effect3}", "add"],
-    ["search device", "{instrument}", "adjust_macro", "{number}"],
-    ["Control {number}", "{speed_modifier}", "{value}"],
-    ["Map {number}", "{action}"],
-    ["Delete", "Map {number}"]
-]
 
-# Templates for generating natural language utterances
-utterance_templates = [
-    "{track_action} track {track_number}",
-    "{device_action} {audio_effect} on track {track_number}",
-    "{device_action} {instrument} with {device_type} on track {track_number}",
-    "{project_action} {track_type} track",
-    "{clip_action} clip {clip_number} on track {track_number}",
-    "Duplicate track {track_number} and add {audio_effect}",
-    "Group tracks {track_number1} and {track_number2} with {audio_effect}",
-    "Create send from track {track_number} to {audio_effect}",
-    "Freeze track {track_number}",
-    "Unfreeze and flatten track {track_number}",
-    "Create new {track_type} track with {instrument} using {device_type}",
-    "Add {audio_effect} to master track",
-    "Create return track with {audio_effect}",
-    "Copy {audio_effect} from track {track_number1} to track {track_number2}",
-    "Group all {instrument} tracks",
-    "Create rack with {audio_effect1}, {audio_effect2}, and {audio_effect3}",
-    "Adjust macro {number} on {instrument}",
-    "Set Control {number} to {value} {speed_modifier}",
-    "Map {number} to {action}",
-    "Delete Map {number}"
-]
+# Action Order Rules:
+# 1. Track creation actions don't include a track number (the track doesn't exist yet)
+# 2. Other track-related actions always start with "track {track_number}"
+# 3. Device-related actions always start with "track {track_number}", "search device", followed by the device name
+# 4. For instruments, the order is: "track {track_number}", "search device", "{instrument}", "{device_type}"
+# 5. Audio effects are treated separately from instruments
+# 6. Value actions include the device name, parameter, action (set/increase/decrease), speed modifier, and value
+# 7. Project actions are global and don't require a track number
+# 8. Clip actions always include a track number and clip number
+# 9. View actions change the current view in Ableton Live and don't require a track number
+# 10. Mapping actions involve assigning controls to parameters and may not require a track number
 
-# Explanation of placeholders:
+# Placeholder Explanations:
+# {track_creation_actions}: Actions for creating new tracks (e.g., add, create)
 # {track_number}: The number of the track (e.g., 1, 2, 3)
-# {track_action}: Any action from the track_actions list
+# {track_actions}: Any action from the track_actions list (e.g., arm, solo, mute)
 # {audio_effect}: Any audio effect from the audio_effects list
-# {device_action}: Any action from the device_actions list
 # {instrument}: Any instrument from the instruments list
 # {device_type}: Any device type from the device_types dictionary for the specified instrument
-# {project_action}: Any action from the project_actions list
-# {track_type}: Any track type from the track_types list
+# {project_actions}: Any action from the project_actions list (e.g., create scene, set tempo)
+# {track_type}: Any track type from the track_types list (e.g., audio, MIDI)
 # {clip_number}: The number of the clip (e.g., 1, 2, 3)
-# {clip_action}: Any action from the clip_actions list
-# {speed_modifier}: Any speed modifier from the speed_modifiers dictionary
+# {clip_actions}: Any action from the clip_actions list (e.g., create, delete, loop)
+# {device_name}: The name of the device being adjusted
+# {parameter}: The parameter of the device being adjusted
+# {value_actions}: Actions that change values (set, increase, decrease)
+# {speed_modifiers}: Any speed modifier from the speed_modifiers dictionary
 # {value}: A numeric value, typically between 0 and 100
-# {action}: Any action that can be mapped (e.g., volume, pan, send level)
-# {number}: A numeric value, typically used for macro or mapping numbers
+# {number}: A numeric value, used for Control or Map operations
+# {view_actions}: Any action from the view_actions list (e.g., show Arranger, show Session)
+# {mapping_actions}: Any action from the mapping_actions list (e.g., Map, Delete Map)
+
+# Action Types and Usage:
+# 1. Track creation actions: 
+#    - Create new tracks
+#    - Don't require a track number
+#
+# 2. Track actions: 
+#    - Performed on specific tracks
+#    - Require a track number
+#
+# 3. Project actions: 
+#    - Global actions that affect the entire project
+#    - Don't require a track number
+#
+# 4. Device actions: 
+#    - Performed on devices within tracks
+#    - Require a track number and device name
+#
+# 5. Clip actions: 
+#    - Performed on clips within tracks
+#    - Require a track number and clip number
+#
+# 6. Value actions:
+#    - Modify parameters of devices on tracks
+#    - Require track number, device name, parameter, action, speed modifier, and value
+#
+# 7. View actions: 
+#    - Change the current view in Ableton Live
+#    - Don't require a track number
+#
+# 8. Mapping actions: 
+#    - Involve assigning controls to parameters
+#    - May or may not require a track number
+
+# Updated action order templates
+action_order_templates = [
+    ["{track_creation_actions}", "{track_type}"],
+    ["track {track_number}", "{track_actions}"],
+    ["track {track_number}", "search device", "{audio_effect}"],
+    ["track {track_number}", "search device", "{instrument}", "{device_type}"],
+    ["{project_actions}"],
+    ["track {track_number}", "clip {clip_number}", "{clip_actions}"],
+    ["track {track_number}", "duplicate", "search device", "{audio_effect}"],
+    ["track {track_number1}", "track {track_number2}", "group"],
+    ["track {track_number}", "create_send", "search device", "{audio_effect}"],
+    ["track master", "search device", "{audio_effect}"],
+    ["track {track_number}", "search device", "{device_name}", "{parameter}", "{value_actions}", "{speed_modifiers}", "{value}"],
+    ["Map {number}", "{parameter}", "{value}"],
+    ["Delete", "Map {number}"],
+    ["{view_actions}"]
+]
+
+# Updated utterance templates
+utterance_templates = [
+    "{track_creation_actions} {track_type} track",
+    "{track_actions} track {track_number}",
+    "Add {audio_effect} to track {track_number}",
+    "Add {instrument} with {device_type} to track {track_number}",
+    "{project_actions}",
+    "{clip_actions} clip {clip_number} on track {track_number}",
+    "Duplicate track {track_number} and add {audio_effect}",
+    "Group tracks {track_number1} and {track_number2}",
+    "Create send from track {track_number} to {audio_effect}",
+    "Add {audio_effect} to master track",
+    "{value_actions} {device_name} {parameter} on track {track_number} to {value} {speed_modifiers}",
+    "Map {number} {parameter} to {value}",
+    "Delete Map {number}",
+    "{view_actions}"
+]
